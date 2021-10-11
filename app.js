eb8694c420e5a742faa4fd996d4a9d134cab96f0
ahmed alhomsi <72552700+ahmed-alhomsi@users.noreply.github.com>
2021-10-11 21:06:54 +0300

    fixed the whole game, no errors, all possibilites are checked, even when the game is tied and no turns are left, it was an amazing first project built from scratch, I learnt alot building this project on my own from the ground up.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let currPlrDisplay = document.getElementById('current-player');
-const game = document.querySelector('game-box');
+const game = document.querySelector('.game-box');
 const restartBtn = document.querySelector('.restart-button');
 const firstRow = document.querySelector('#first-row');
 const secondRow = document.querySelector('#second-row');
@@ -25,8 +25,6 @@ const thirdColumn = [columnThreeOne, columnThreeTwo, columnThreeThree];
 const firstDiagonal = [diagonalOneOne, diagonalOneTwo, diagonalOneThree];
 const secondDiagonal = [diagonalTwoOne, diagonalTwoTwo, diagonalTwoThree];
 let turnsLeft = 9;
-let gameEnded = false;
-
 let currentPlayer = 1;
 currPlrDisplay.innerText = ': X';
 
@@ -34,18 +32,21 @@ function checkIfGameIsWon() {
     /* check if any one of the players have won */
     //check rows
     const rowsArr = [firstRow, secondRow, thirdRow];
-    for(let i = 0; i < rowsArr.length - 1; i++) {
+    for(let i = 0; i < rowsArr.length; i++) {
         checkRows(rowsArr[i]);
     }
 
     //check columns
-    checkColumns();
+    const columnsArr = [firstColumn, secondColumn, thirdColumn];
+    for(let i = 0; i < columnsArr.length; i++) {
+        checkColumns(columnsArr[i]);
+    }
 
     //check diagonal lines
-    checkDiagonals();
-    
-    //if game has ended and no one has won, stop the game
-    endGame();
+    const diagonalsArr = [firstDiagonal, secondDiagonal];
+    for(let i = 0; i < diagonalsArr.length; i++) {
+        checkDiagonals(diagonalsArr[i]);
+    }
 }
 
 function checkRows(currentRow) {
@@ -68,51 +69,41 @@ function checkRows(currentRow) {
     }
 }
 
-function checkColumns() {
-    const columnsArr = [firstColumn, secondColumn, thirdColumn];
-    for(let i = 0; i < 3; i++) {
-        for(let j = 0; j < 3; j++) {
-            console.log(columnsArr[j][i].classList.contains('x'))
-            if(columnsArr[j][i].classList.contains('x') && columnsArr[j][i+1].classList.contains('x') && columnsArr[j][i+2].classList.contains('x')) {
-                playerWon('x');
-            } else if(columnsArr[j][i].classList.contains('o') && columnsArr[j][i+1].classList.contains('o') && columnsArr[j][i+2].classList.contains('o')) {
-                playerWon('o');
-            }
+function checkColumns(currentColumn) {
+    let columnArr = [];
+    for(let i = 0; i < currentColumn.length; i++) {
+        if(currentColumn[i].classList.contains('x')) {
+            columnArr.push('x');
+        } else if(currentColumn[i].classList.contains('o')) {
+            columnArr.push('o');
         }
     }
-
-//     console.log(currentColumn[0].classList.contains('x'));
-//     let columnArr = [];
-//     for(let i = 0; i < currentColumn[1].children.length; i++) {
-//         if(currentColumn.children[i].classList.contains('x')) {
-//             columnArr.push('x');
-//         } else if(currentColumn.children[i].classList.contains('o')){
-//             columnArr.push('o');
-//         } else {
-//             columnArr = [];
-//         }
-//     }
-//     for(let i = 0; i < columnArr.length; i++) {
-//         if(columnArr[i] === 'x' && columnArr[i+1] === 'x' && columnArr[i+2] === 'x') {
-//             playerWon('x');
-//         } else if(columnArr[i] === 'o' && columnArr[i+1] === 'o' && columnArr[i+2] === 'o') {
-//             playerWon('o');
-//         }
-//     }
+    for(let i = 0; i < currentColumn.length; i++) {
+        if(columnArr[i] === 'x' && columnArr[i+1] === 'x' && columnArr[i+2] === 'x') {
+            playerWon('x');
+        } else if(columnArr[i] === 'o' && columnArr[i+1] === 'o' && columnArr[i+2] === 'o') {
+            playerWon('o');
+        }
+    }
 }
 
-// function checkDiagonals() {
-//     const diagonalsArr = [firstDiagonal, secondDiagonal];
-//     for(let i = 0; i < 3; i++) {
-//         for(let j = 0; j < 3; j++) {
-//             if(diagonalsArr[j][i].classList.contains('x') && diagonalsArr[j][i+1].classList.contains('x') && diagonalsArr[j][i+2].classList.contains('x')) {
-//                 playerWon('x');
-//             } else if(diagonalsArr[j][i].classList.contains('o') && diagonalsArr[j][i+1].classList.contains('o') && diagonalsArr[j][i+2].classList.contains('o')) {
-//                 playerWon('o');
-//             }
-//         }
-//     }
-// }
+function checkDiagonals(currentDiagonal) {
+    let diagonalArr = [];
+    for(let i = 0; i < currentDiagonal.length; i++) {
+        if(currentDiagonal[i].classList.contains('x')) {
+            diagonalArr.push('x');
+        } else if(currentDiagonal[i].classList.contains('o')) {
+            diagonalArr.push('o');
+        }
+    }
+    for(let i = 0; i < currentDiagonal.length; i++) {
+        if(diagonalArr[i] === 'x' && diagonalArr[i+1] === 'x' && diagonalArr[i+2] === 'x') {
+            playerWon('x');
+        } else if(diagonalArr[i] === 'o' && diagonalArr[i+1] === 'o' && diagonalArr[i+2] === 'o') {
+            playerWon('o');
+        }
+    }
+}
 
 function switchPlayer(target) {
     /* switch the current player */
@@ -132,9 +123,9 @@ function playerWon(player) {
     window.location.reload();
 }
 
-function endGame() {
+function checkIfGameIsTied() {
     if(turnsLeft === 1) {
-        alert('Game Over, Tie!!');
+        alert('GameOver, A Tie!!');
     }
 }
 
@@ -151,19 +142,10 @@ document.addEventListener('click', (e)=>{
                 switchPlayer(e.target);
                 checkIfGameIsWon();
             }
+            checkIfGameIsTied();
+            turnsLeft--;
         }
     }
 });
 
 restartBtn.addEventListener('click',()=>{window.location.reload()});
-
-
-
-// if(!checkColumns() && !checkRows && !checkDiagonals) {
-//     alert('Game Over, No one won!');
-//     window.location.reload();
-// }
-
-
-/* i need to check if player has won at every click, by seeing all 8 possibilites, 2 diagonal lines | 3 row possibilites | 3 column possibilites */
-/* end game after all boxes are full and neither of the players have won */
